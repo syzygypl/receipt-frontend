66dde974c426eb45319ef6a71a17d429396ed7cf
Michal Saletra <michal.saletra@Michals-MacBook-Pro.local>
2019-06-08 13:55:42 +0200

    login view created

--- src/Components/Button/index.tsx
+++
@@ -1,9 +0,0 @@
-import React, {FunctionComponent} from 'react'
-interface ButtonProps {
-  text: string
-}
-const Button: FunctionComponent<ButtonProps> = (props) => {
-  return (
-    <div>Button</div>
-  )
-}
--- src/views/LoginScreen/index.tsx
+++ src/views/LoginScreen/index.tsx
@@ -1,9 +1,48 @@
 import React from 'react'
+ import {Button, Typography, Container, FormControl, InputLabel, Input} from '@material-ui/core';
+import styles from './styles.module.scss';
 
-const LoginScreen = () => {
-  return (
-    <div><h1>LoginScreen</h1></div>
-  )
+interface LoginScreenState {
+  inputValue: string;
+}
+interface LoginScreenProps {
+  eventName: string;
 }
 
-export default LoginScreen
+class LoginScreen extends React.Component<LoginScreenProps, LoginScreenState> {
+  state = {
+    inputValue: ''
+  }
+  onInputChange = (event:any):void => {
+    this.setState({
+      inputValue: event.target.value
+    })
+  }
+  handleLoginClick = ():void => {
+    // TO DO preprare login action
+    console.log('nie ma')
+  }
+  render() {
+    return (
+      <Container maxWidth='lg' className={styles['login-screen']}>
+        <Typography variant='h3' gutterBottom>Cześć!</Typography>
+        {/* TODO: add event name from props */}
+        <Typography variant='subtitle1'>{`Żeby przejść do edycji wydarzenia jakieś wydarzenie`}</Typography>
+        <FormControl className={styles['login-screen__form']}>
+          <InputLabel htmlFor='login-input'>Login:</InputLabel>
+          <Input id='login-input' value={this.state.inputValue} onChange={this.onInputChange}/>
+          <div className={styles['login-button']}>
+          <Button
+            fullWidth
+            variant='contained'
+            type='submit' 
+            size='medium' 
+            onClick={this.handleLoginClick}  
+            color='primary'>Login</Button>
+          </div>
+        </FormControl>
+      </Container>
+    )
+  }
+}
+export default LoginScreen;
