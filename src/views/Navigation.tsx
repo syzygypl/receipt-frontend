6384401d86e30a7caf4595338fafc2eef1c8ec10
Krzysztof Hamerszmit <krzysztof.hamerszmit@syzygy.pl>
2019-06-08 12:23:31 +0200

    Add mising nav link

--- src/views/Navigation.tsx
+++ src/views/Navigation.tsx
@@ -1,27 +1,38 @@
-import React from 'react'
-import {Link as RouterLink} from 'react-router-dom'
-import Link from '@material-ui/core/Link'
+import React from "react";
+import { Link as RouterLink } from "react-router-dom";
+import Link from "@material-ui/core/Link";
+
+const navStyles = {
+  display: "flex",
+  justifyContent: "space-around",
+  borderBottom: "solid 1px grey",
+  padding: '10px',
+};
 
 const Navigation = () => {
   return (
-    <div style={{display: 'flex', justifyContent: 'space-around'}}>
+    <div style={navStyles}>
       <Link>
-        <RouterLink to='/'>Welcome Page</RouterLink>
+        <RouterLink to="/">Welcome Page</RouterLink>
       </Link>
 
       <Link>
-        <RouterLink to='/event-create'>Create event</RouterLink>
+        <RouterLink to="/login">Login</RouterLink>
       </Link>
 
       <Link>
-        <RouterLink to='/event-edit'>Edit event</RouterLink>
+        <RouterLink to="/event-create">Create event</RouterLink>
       </Link>
 
       <Link>
-        <RouterLink to='/payment'>Payment</RouterLink>
+        <RouterLink to="/event-edit">Edit event</RouterLink>
+      </Link>
+
+      <Link>
+        <RouterLink to="/payment">Payment</RouterLink>
       </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
